a06687dd719e7ef4938a49313e1ea1f0719e5ede
Christian Rodriguez <xchristianz@gmail.com>
2017-08-19 15:04:39 -0700

    initial commit

---
+++ Databases in Python/diary.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from peewee import *
+import datetime
+
+db = SqliteDatabase('diary.db')
+
+
+class Entry(Model):
+	content = TextField()
+	timestamp = DateTimeField(default=datetime.datetime.now)
+
+	class Meta:
+		database = db
+
+def initialize():
+	"""Create the database and table if they don't exist"""
+	db.connect()
+	db.create_tables([Entry], safe = True)
+
+def menu_loop():
+	"""Show the menu"""
+
+def add_entry():
+	"""Add an entry"""
+
+def view_entries():
+	"""View previous entries"""
+
+def delete_entry():
+	"""Delete an entry"""
+
+
+if __name__ == '__main__':
+	initialize()
+	menu_loop()
